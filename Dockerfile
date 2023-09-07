b9fadf1a8a4da5a4ccc5dd8ddb0071c8985b7524
Mily <milyth@duck.com>
2023-09-07 11:36:57 -0300

    feat: basic docker image

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+FROM alpine:3.18
+
+RUN apk add --no-cache git wget
+RUN wget https://ziglang.org/download/0.11.0/zig-linux-x86_64-0.11.0.tar.xz
+
+RUN tar -xf zig-linux-x86_64-0.11.0.tar.xz -C /var/tmp
+RUN mv /var/tmp/zig-linux-x86_64-0.11.0 /usr/local/zig
+RUN ln -s /usr/local/zig/zig /usr/local/bin/zig
+
+COPY . .
+WORKDIR .
+RUN git submodule init && git submodule update
+RUN zig build -Doptimize=ReleaseFast --verbose
+COPY origin/files/fib.json /var/rinha/source.rinha.json
+ENTRYPOINT ["zig-out/bin/touka", "/var/rinha/source.rinha.json"]
+
--- src/main.zig
+++ src/main.zig
@@ -1,3 +1,24 @@
 const std = @import("std");
+const json = std.json;
+const fs = std.fs;
 
-pub fn main() !void {}
+pub fn main() !void {
+    var general_purpose_allocator = std.heap.GeneralPurposeAllocator(.{}){};
+    const gpa = general_purpose_allocator.allocator();
+    defer std.debug.assert(general_purpose_allocator.deinit() == .ok);
+
+    var argv = std.process.args();
+    _ = argv.next();
+
+    const fileName = argv.next() orelse {
+        std.debug.panic("I need a json file bro.", .{});
+    };
+
+    const absolutePath = try std.fs.realpathAlloc(gpa, fileName);
+    defer gpa.free(absolutePath);
+
+    const inputFile = try fs.openFileAbsolute(absolutePath, .{ .mode = std.fs.File.OpenMode.read_only });
+    defer inputFile.close();
+
+    _ = json.reader(gpa, inputFile);
+}
